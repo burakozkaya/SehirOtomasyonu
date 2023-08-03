b76bd276c66154fde74412ba01e4d43e6ad5a059
Burak Özkaya <ozkayaburak97@gmail.com>
2023-08-03 12:41:46 +0300

    Add project files.

---
+++ WinForm2/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WinForm2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label3 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            comboBox2 = new ComboBox();
+            comboBox1 = new ComboBox();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(33, 139);
+            label3.Name = "label3";
+            label3.Size = new Size(25, 15);
+            label3.TabIndex = 20;
+            label3.Text = "İlçe";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(33, 92);
+            label2.Name = "label2";
+            label2.Size = new Size(13, 15);
+            label2.TabIndex = 19;
+            label2.Text = "İl";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(111, 197);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 18;
+            button1.Text = "Ekle";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // comboBox2
+            // 
+            comboBox2.Enabled = false;
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(88, 136);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(121, 23);
+            comboBox2.TabIndex = 17;
+            // 
+            // comboBox1
+            // 
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.ForeColor = SystemColors.WindowText;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "+" });
+            comboBox1.Location = new Point(88, 89);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 16;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged_1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(88, 34);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(121, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(243, 255);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Otomasyon v1";
+            KeyDown += Form1_KeyDown;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label3;
+        private Label label2;
+        private Button button1;
+        private ComboBox comboBox2;
+        private ComboBox comboBox1;
+        private TextBox textBox1;
+    }
+}
---
+++ WinForm2/Form1.cs
@@ -0,0 +1,67 @@
+﻿namespace WinForm2
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, List<string>> dictionary;
+        public Form1()
+        {
+            dictionary = new Dictionary<string, List<string>>();
+            InitializeComponent();
+        }
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string temp = textBox1.Text.Trim().ToLower();
+            if (temp == string.Empty)
+            {
+                MessageBox.Show("Alan boş bırakılamaz");
+                return;
+            }
+            if (dictionary.ContainsKey(temp))
+            {
+                MessageBox.Show($"{temp} ili listede bulunmakta.");
+                return;
+            }
+            if (comboBox1.SelectedItem == "+" || comboBox1.SelectedIndex == -1)
+            {
+                dictionary.Add(temp, new List<string>());
+                MessageBox.Show($"{temp} il olarak listeye eklendi.");
+                comboBox1.Items.Add(temp);
+            }
+            else
+            {
+                if (dictionary[comboBox1.SelectedItem.ToString()].Contains(temp))
+                {
+                    MessageBox.Show($"{comboBox1.SelectedItem} ili {temp} adlı ilçeye sahip.");
+                    return;
+                }
+                comboBox2.Items.Add(temp);
+                MessageBox.Show($"{comboBox1.SelectedItem} iline {temp} ilçesi eklendi.");
+                dictionary[comboBox1.SelectedItem.ToString()].Add(temp);
+            }
+            textBox1.Clear();
+
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != "+")
+            {
+                comboBox2.Enabled = true;
+                comboBox2.Items.Clear();
+                foreach (var item in dictionary[comboBox1.SelectedItem.ToString()])
+                {
+                    comboBox2.Items.Add(item.ToString());
+                }
+            }
+            else
+                comboBox2.Enabled = false;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show(e.KeyValue.ToString());
+        }
+    }
+}
